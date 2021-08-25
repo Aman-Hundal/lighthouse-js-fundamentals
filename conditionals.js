15074cf2d18506d1f82dd6b77e729a31b56b2b2c
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-08-25 01:08:05 +0000

    Add more condional exmaples

--- conditionals.js
+++ conditionals.js
@@ -17,7 +17,7 @@ if (cold) {
   console.log("Short sleeves are all good!");
 } */
 
-const temp = 25;
+const temp = -45;
 
 
 if (temp <= 0) {
@@ -26,4 +26,26 @@ if (temp <= 0) {
   console.log("Short sleeves wont cut it");
 } else {
   console.log("Short sleeves are fine");
+}
+
+const isCitizen = true;
+const age = 30
+
+if (age >= 18 && isCitizen) {
+  console.log("You are elligible to vote.")
+} else if (isCitizen===false) {
+  console.log("Sorry you are not a citizen. Please apply for citizenship.");
+} else {
+  let ageDiff = 18-age;
+  console.log(`Sorry, you must wait atleast ${ageDiff} years to vote`);
+}
+
+if (temp <-40 || temp >40) {
+  console.log("Maybe going outside isnt such a good idea");
+}
+
+let raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home")
 }
