f3678fecd9e779442cf1b0bce036e9da894cdafe
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-08-26 08:28:04 +0000

    Using an isEven function

---
+++ even.js
@@ -0,0 +1,14 @@
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+let tenIsEven = isEven(10);
+let elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+console.log(20 % 2 === 0);
+
+console.log(isEven(12));
+console.log(isEven(99));
