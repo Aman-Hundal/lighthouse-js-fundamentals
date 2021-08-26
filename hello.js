ff5b7922c53b2d49e7b1c4dc1cdc51e013f6c3c3
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-08-26 08:12:11 +0000

    Use of functions as greeting tools

---
+++ hello.js
@@ -0,0 +1,15 @@
+// function expression
+const sayHello = function (name) {
+  console.log(`Hello ${name}`);
+}
+
+sayHello("Amo");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+// normal function
+function sayHola (name) {
+  console.log(`Hola ${name}`);
+}
+
+sayHola("Amo");
